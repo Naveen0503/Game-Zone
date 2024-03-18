25a5230b5a44f820cf50b76a382beea1d29c7ba3
Naveen0503 <109223016+Naveen0503@users.noreply.github.com>
2024-03-18 10:48:15 +0530

    Update dashboard.js

--- src/Games/dashboard.js
+++ src/Games/dashboard.js
@@ -14,6 +14,7 @@ function Dashboard() {
   const nav = useNavigate();
   const handleBack = () => {
     nav("/");
+    window.location.reload();
   }
     return (
         <div className="dashboard">
